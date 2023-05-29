a17d46b728094b6e285a308a27d06f15e9fb54df
C3sar01 <cesar.cssoto@gmail.com>
2023-05-29 18:04:14 -0400

    cambios en PDF

--- src/main/java/com/mindhub/homebanking/utils/PDFGenerator.java
+++ src/main/java/com/mindhub/homebanking/utils/PDFGenerator.java
@@ -1,16 +1,18 @@
 package com.mindhub.homebanking.utils;
 
-import com.lowagie.text.*;
 import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
-
 import com.lowagie.text.pdf.PdfWriter;
 import com.mindhub.homebanking.dtos.TransactionDTO;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class PDFGenerator {
@@ -21,19 +23,25 @@ public class PDFGenerator {
         this.listTransactionDTO = listTransactionDTO;
     }
 
-
     public byte[] export() throws DocumentException, IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
 
         Document document = new Document(PageSize.A4);
         PdfWriter.getInstance(document, byteArrayOutputStream);
 
+        String logoPath = getClass().getClassLoader().getResource("static/web/img/innovbank.png").getPath();
+
         document.open();
+
+        Image logo = Image.getInstance(logoPath);
+        logo.scaleToFit(100, 100);
+        logo.setAbsolutePosition(50, 750);
+
+        document.add(logo);
 
         Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
         fontTitle.setSize(18);
-        fontTitle.setColor(Color.BLUE);
+        fontTitle.setColor(Color.MAGENTA);
 
         Paragraph title = new Paragraph("Transactions", fontTitle);
         title.setAlignment(Paragraph.ALIGN_CENTER);
@@ -57,7 +65,7 @@ public class PDFGenerator {
 
     private void writeTableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(Color.BLUE);
+        cell.setBackgroundColor(Color.MAGENTA);
         cell.setPadding(5);
 
         Font font = FontFactory.getFont(FontFactory.HELVETICA);
@@ -81,10 +89,14 @@ public class PDFGenerator {
     }
 
     private void writeTableData(PdfPTable table) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         for (TransactionDTO transactionDTO : listTransactionDTO) {
             table.addCell(String.valueOf(transactionDTO.getId()));
             table.addCell(String.valueOf(transactionDTO.getType()));
-            table.addCell(String.valueOf(transactionDTO.getDate()));
+            LocalDateTime date = transactionDTO.getDate();
+            String formattedDate = date.format(formatter);
+            table.addCell(formattedDate);
             table.addCell(String.valueOf(transactionDTO.getDescription()));
             table.addCell(String.valueOf(transactionDTO.getAmount()));
         }
