840c7cc66b9187c92edd3cfd60b7d3ce1efa34ef
C3sar01 <cesar.cssoto@gmail.com>
2023-05-29 22:18:37 -0400

    Ajuste en cardUtils

--- src/main/java/com/mindhub/homebanking/utils/CardUtils.java
+++ src/main/java/com/mindhub/homebanking/utils/CardUtils.java
@@ -5,10 +5,14 @@ public final class CardUtils {
     private CardUtils(){}
 
     public static String getRandomNumber(int min, int max) {
-        return  (int)((Math.random() * (max - min)) + min ) + "-";
+        return  (int)((Math.random() * (max - min)) + min ) + "-"
+                + ((Math.random() * (max - min)) + min ) + "-"
+                +((Math.random() * (max - min)) + min ) + "-"
+                +((Math.random() * (max - min)) + min );
     }
 
     public static int getRandomNumberCvv(int min, int max) {
+
         return (int) ((Math.random() * (max - min)) + min);
     }
 }
